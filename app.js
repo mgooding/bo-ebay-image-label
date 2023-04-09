844e02fc0e0c53ec0623c82850b75a1bb23fa3f4
Matt Gooding <matt.gooding@gmail.com>
2023-04-09 13:32:44 -0400

    Thank you ChatGPT

--- app.js
+++ app.js
@@ -1,64 +1,166 @@
-const imageInput = document.getElementById('image-input');
-const labelInput = document.getElementById('label-input');
-const generateBtn = document.getElementById('generate-btn');
-const downloadLink = document.getElementById('download-link');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const imageInput = document.getElementById("image-input");
+const labelInput = document.getElementById("label-input");
+const downloadLink = document.getElementById("download-link");
+const outputImage = document.getElementById("output-image");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-imageInput.addEventListener('change', () => {
+labelInput.value = `vert: 5 mA/div
+hori: 5 v/ div
+0.05 mA/ steps
+`;
+
+let originalImage;
+
+function getScaledDimensions(width, height, maxWidth, maxHeight) {
+  const aspectRatio = width / height;
+  let newWidth = width;
+  let newHeight = height;
+
+  if (width > maxWidth) {
+    newWidth = maxWidth;
+    newHeight = newWidth / aspectRatio;
+  }
+
+  if (newHeight > maxHeight) {
+    newHeight = maxHeight;
+    newWidth = newHeight * aspectRatio;
+  }
+
+  return { width: newWidth, height: newHeight };
+}
+
+function resizeImage(inputImage) {
+  const { width, height } = getScaledDimensions(
+    inputImage.width,
+    inputImage.height,
+    800,
+    800
+  );
+  const resizedCanvas = createCanvas(width, height);
+  const resizedCtx = resizedCanvas.getContext("2d");
+  resizedCtx.drawImage(inputImage, 0, 0, width, height);
+  const outputImage = new Image();
+  outputImage.src = resizedCanvas.toDataURL("image/jpeg");
+  return outputImage;
+}
+
+function createCanvas(width, height) {
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  return canvas;
+}
+
+imageInput.addEventListener("change", () => {
   if (imageInput.files && imageInput.files[0]) {
     const reader = new FileReader();
-    reader.onload = e => {
-      const img = new Image();
-      img.src = e.target.result;
-      img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
+    reader.onload = (e) => {
+      const rawImage = new Image();
+      rawImage.src = e.target.result;
+      rawImage.onload = () => {
+        originalImage = resizeImage(rawImage);
+        inputChanged = true;
       };
     };
     reader.readAsDataURL(imageInput.files[0]);
   }
 });
 
-generateBtn.addEventListener('click', async () => {
+async function updateImage() {
   const labelText = labelInput.value.trim();
-  if (!labelText) {
-    alert('Please enter some label text.');
-    return;
+
+  if (originalImage) {
+    canvas.width = originalImage.width;
+    canvas.height = originalImage.height;
+    ctx.drawImage(originalImage, 0, 0);
   }
-  
-  // Draw the label text on the canvas
-  ctx.font = '20px Arial';
-  ctx.fillStyle = 'white';
-  ctx.strokeStyle = 'black';
+
+  drawLabelOnCanvas(labelText);
+  updateDownloadLink();
+  displayOutputImage();
+}
+
+function drawLabelOnCanvas(labelText) {
+  const lines = labelText.split(/\r?\n/);
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
   ctx.lineWidth = 4;
   const maxWidth = canvas.width - 20;
   const lineHeight = 25;
-  const words = labelText.split(' ');
-  let line = '';
-  let y = 30;
+
+  const totalTextHeight = lines.length * lineHeight;
+  let y = canvas.height - totalTextHeight;
+
+  for (const textLine of lines) {
+    y = drawLineOfText(textLine, maxWidth, lineHeight, y);
+  }
+}
+
+function drawLineOfText(textLine, maxWidth, lineHeight, y) {
+  const words = textLine.split(" ");
+  let line = "";
 
   for (const word of words) {
-    const testLine = line + word + ' ';
+    const testLine = line + word + " ";
     const metrics = ctx.measureText(testLine);
     const testWidth = metrics.width;
+
     if (testWidth > maxWidth) {
-      ctx.strokeText(line, 10, y);
-      ctx.fillText(line, 10, y);
-      line = word + ' ';
+      drawText(line, 10, y);
+      line = word + " ";
       y += lineHeight;
     } else {
       line = testLine;
     }
   }
-  ctx.strokeText(line, 10, y);
-  ctx.fillText(line, 10, y);
+  drawText(line, 10, y);
+  return y + lineHeight;
+}
 
-  // Convert the canvas to a JPG
-  const dataURL = canvas.toDataURL('image/jpeg');
+function drawText(text, x, y) {
+  ctx.strokeText(text, x, y);
+  ctx.fillText(text, x, y);
+}
+
+function updateDownloadLink() {
+  const dataURL = canvas.toDataURL("image/jpeg");
   downloadLink.href = dataURL;
   downloadLink.hidden = false;
-});
-    
+}
 
+function displayOutputImage() {
+  const scaledDims = getScaledDimensions(
+    originalImage.width,
+    originalImage.height,
+    window.innerWidth * 0.45,
+    window.innerHeight * 0.45
+  );
+
+  outputImage.width = scaledDims.width;
+  outputImage.height = scaledDims.height;
+  outputImage.src = canvas.toDataURL("image/jpeg");
+  outputImage.hidden = false;
+}
+
+let inputChanged = false;
+
+imageInput.addEventListener("input", () => {
+  inputChanged = true;
+});
+
+labelInput.addEventListener("input", () => {
+  inputChanged = true;
+});
+
+window.addEventListener("resize", () => {
+  inputChanged = true;
+});
+
+setInterval(async () => {
+  if (inputChanged) {
+    await updateImage();
+    inputChanged = false;
+  }
+}, 1000);
